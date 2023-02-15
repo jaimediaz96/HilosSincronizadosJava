99a55049ecd8232d15048fbe457a67795196daed
JaimeDiaz <jedt1996@gmail.com>
2023-02-15 13:01:33 -0500

    Hilos asincronos

--- src/hilossincronizados/HilosSincronizados.java
+++ src/hilossincronizados/HilosSincronizados.java
@@ -6,14 +6,21 @@ package hilossincronizados;
  */
 public class HilosSincronizados {
 
-    public HilosSincronizados() {
-        
-    }
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        HilosVarios hilo1 = new HilosVarios();
+        hilo1.start();
+        HilosVarios hilo2 = new HilosVarios();
+        hilo2.start();
+        HilosVarios hilo3 = new HilosVarios();
+        hilo3.start();
+        HilosVarios hilo4 = new HilosVarios();
+        hilo4.start();
+        HilosVarios hilo5 = new HilosVarios();
+        hilo5.start();
+        HilosVarios hilo6 = new HilosVarios();
+        hilo6.start();
     }
 }
--- src/hilossincronizados/HilosVarios.java
+++ src/hilossincronizados/HilosVarios.java
@@ -1,12 +1,23 @@
 package hilossincronizados;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author JAIME EDUARDO DIAZ TOBON
  */
-public class HilosVarios {
+public class HilosVarios extends Thread {
     
-    public HilosVarios() {
-        
+    @Override
+    public void run() {
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Ejecutando hilo: " + getName());
+            try {
+                Thread.sleep(700);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(HilosVarios.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
