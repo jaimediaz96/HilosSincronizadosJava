6c0da6b8c955c3dcc7612f8bd92b8b5da9c4af37
JaimeDiaz <jedt1996@gmail.com>
2023-02-15 13:05:55 -0500

    Hilos sincronizados con el metodo join

--- src/hilossincronizados/HilosSincronizados.java
+++ src/hilossincronizados/HilosSincronizados.java
@@ -1,5 +1,8 @@
 package hilossincronizados;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author JAIME EDUARDO DIAZ TOBON
@@ -12,15 +15,46 @@ public class HilosSincronizados {
     public static void main(String[] args) {
         HilosVarios hilo1 = new HilosVarios();
         hilo1.start();
+        try {
+            hilo1.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(HilosSincronizados.class.getName()).log(Level.SEVERE, null, ex);
+        }
         HilosVarios hilo2 = new HilosVarios();
         hilo2.start();
+        try {
+            hilo2.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(HilosSincronizados.class.getName()).log(Level.SEVERE, null, ex);
+        }
         HilosVarios hilo3 = new HilosVarios();
         hilo3.start();
+        try {
+            hilo3.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(HilosSincronizados.class.getName()).log(Level.SEVERE, null, ex);
+        }
         HilosVarios hilo4 = new HilosVarios();
         hilo4.start();
+        try {
+            hilo4.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(HilosSincronizados.class.getName()).log(Level.SEVERE, null, ex);
+        }
         HilosVarios hilo5 = new HilosVarios();
         hilo5.start();
+        try {
+            hilo5.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(HilosSincronizados.class.getName()).log(Level.SEVERE, null, ex);
+        }
         HilosVarios hilo6 = new HilosVarios();
         hilo6.start();
+        try {
+            hilo6.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(HilosSincronizados.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Hilo del main");
     }
 }
--- src/hilossincronizados/HilosVarios.java
+++ src/hilossincronizados/HilosVarios.java
@@ -14,7 +14,7 @@ public class HilosVarios extends Thread {
         for (int i = 0; i < 15; i++) {
             System.out.println("Ejecutando hilo: " + getName());
             try {
-                Thread.sleep(700);
+                Thread.sleep(400);
             } catch (InterruptedException ex) {
                 Logger.getLogger(HilosVarios.class.getName()).log(Level.SEVERE, null, ex);
             }
